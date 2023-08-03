ed42609c96b72f74a15358910a66d5f1c5407150
Julian De Maio <julidema82@gmail.com>
2023-08-03 17:19:23 -0300

    comenzando peliculaController

--- cine/src/main/java/com/example/cine/Pelicula.java
+++ cine/src/main/java/com/example/cine/Pelicula.java
@@ -1,12 +1,19 @@
 package com.example.cine;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
 @Document(collection = "peliculas")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pelicula {
     @Id
     private ObjectId id;
@@ -14,6 +21,7 @@ public class Pelicula {
     private String trailerLink;
     private String poster;
     private List<String> backdrops;
+    @DocumentReference
     private List<Review> reviewIds;
     private String fechaEstreno;
     private List<String> generos;
